c8bd6fc8db07cf513ce3bb3cb05c34ad47ca41b4
abhishek36 <akamble9913@gmail.com>
2022-02-04 23:33:31 +0530

    first commit

---
+++ src/controllers/blog.controller.js
@@ -0,0 +1,35 @@
+//import Blog model here
+
+const addBlog = async (req, res) => {
+  //addBlog api logic here
+};
+
+const getAllBlog = async (req, res) => {
+  //getAllBlog api logic here
+};
+
+const getBlogByUserId = async (req, res) => {
+  //getBlogByUserId api logic here
+};
+
+const getBlogById = async (req, res) => {
+  //getBlogById api logic here
+};
+
+const updateBlogById = async (req, res) => {
+  //updateBlogById api logic here
+};
+
+const deleteBlogById = async (req, res) => {
+  //deleteBlogById api logic here
+};
+
+const BlogController = {
+  addBlog,
+  getAllBlog,
+  getBlogByUserId,
+  getBlogById,
+  updateBlogById,
+  deleteBlogById,
+};
+module.exports = BlogController;
---
+++ src/controllers/bookmark.controller.js
@@ -0,0 +1,15 @@
+//import Bookmark model here
+
+const addOrRemoveBookmark = async (req, res) => {
+  //addOrRemoveBookmark api logic here
+};
+
+const getAllBookmark = async (req, res) => {
+  //getAllBookmark api logic here
+};
+
+const BookmarkController = {
+  addOrRemoveBookmark,
+  getAllBookmark,
+};
+module.exports = BookmarkController;
